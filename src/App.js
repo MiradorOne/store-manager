b2ad7d6730f80d84bf92cad95ec5fd405e4cee18
kitdemonio <ddd48533@gmail.com>
2016-11-25 22:20:58 +0100

    Fix font-awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import makeQuery from './api/connection';
-
 
 import Sidebar from './app/components/Sidebar/Sidebar';
 import Dashboard from './app/components/Dashboard/Dashboard';
@@ -8,15 +6,11 @@ import Dashboard from './app/components/Dashboard/Dashboard';
 export default class Main extends Component {
     constructor() {
         super();
-        this.state = {
-            data: makeQuery('SELECT * FROM offices')
-        }
     }
 
     render() {
         return (
             <div className="">
-                {console.log(this.state.data)}
                 <Sidebar/>
                 <Dashboard/>
             </div>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
