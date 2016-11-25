7d31f96aa213988a02022647547a9fcd4de4bc20
kitdemonio <ddd48533@gmail.com>
2016-11-25 22:30:43 +0100

    Delete redundant component & add router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
 import App from './App';
 import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-    <App />,
+    <Router history={browserHistory}>
+        <Route path="/" component={App}/>
+    </Router>,
     document.getElementById('root')
 );
