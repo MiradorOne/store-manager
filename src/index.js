3caede031c0847db5ffa35935ece87a608f5e184
kitdemonio <ddd48533@gmail.com>
2016-11-25 13:07:56 +0100

    Initial structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
+import './index.scss';
+
+import App from './app/App';
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+  </Router>,
+  document.getElementById('root')
+);
