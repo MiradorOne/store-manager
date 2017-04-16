0a3b4f9eae0c973993255c94072e9d5a68ed98de
kitdemonio <ddd48533@gmail.com>
2017-04-17 00:01:50 +0200

    Improve error handling

--- server/data.js
+++ server/data.js
@@ -1,5 +1,7 @@
 const connection = require('./connection');
 const config = require('./connection.config');
+const log = require('winston');
+const validator = require('validator');
 
 function Data() {
     this.get = function (req, res) {
@@ -35,12 +37,14 @@ function Data() {
                         ${queryOptions.where ? 'where ' + queryOptions.where : ''}`;
             }
 
-            console.log(sql);
+            log.info(sql);
 
             con.query(sql, function (err, result) {
                 if (err) {
-                    res.status(404);//TODO: Error handling
-                    return res.sendStatus('Error in SQL query')
+                    con.release();
+                    res.status(400);
+                    res.send('Wrong query');
+                    return log.error(err);
                 }
                 con.release();
                 res.send(result);
@@ -50,72 +54,96 @@ function Data() {
 
     this.delete = function (req, res) {
         connection.acquire(function (err, con) {
-            if (!req.params.number) {
-                res.status(403);//
-                return res.sendStatus('Access denied')
-            } else {
-                const sql ='DELETE FROM '+ config.database + '.customers WHERE customers.customerNumber = ' + req.params.number;
-                con.query(sql,
-                    function (err) {
-                        if (err) {
-                            console.log(sql);
-                            con.release();
-                            res.send({success: false, error: err});
-                        }
-                        con.release();
-                        res.send('Customer №'+ req.params.number + ' successfully deleted');
-                    }
-                )
+
+            log.info('DELETE FROM ' + config.database + '.customers WHERE customers.customerNumber = ' + con.escape(req.params.number));
+
+            if (!validator.isNumeric(req.params.number)) {
+                res.status(400);
+                res.send('Enter a number!');
+                con.release();
+                return log.error(err);
             }
+
+            const sql = 'DELETE FROM ' + config.database + '.customers WHERE customers.customerNumber = ' + con.escape(req.params.number);
+            con.query(sql,
+                function (err) {
+                    if (err) {
+                        con.release();
+                        res.send({success: false, error: err});
+                        return log.error(err);
+                    }
+                    con.release();
+                    res.send('Customer №' + req.params.number + ' successfully deleted');
+                }
+            )
         })
     };
 
     this.update = function (req, res) {
         connection.acquire(function (err, con) {
-            if (!req.params.number) {
-                res.status(403);//
-                return res.sendStatus('Access denied')
+
+            let sql = '';
+
+            if (validator.isNumeric(req.params.number)) {
+
+                sql = 'UPDATE ' + config.database + '.customers ' +
+                    'SET customerName = ' + '"' + req.query.name + '"' + ', ' +
+                    'contactLastName = ' + '"' + req.query.lastname + '"' + ', ' +
+                    'contactFirstName = ' + '"' + req.query.firstname + '"' + ', ' +
+                    'phone = ' + '"' + req.query.phone + '"' + ' ' +
+                    'WHERE customers.customerNumber = ' + req.params.number + ';';
+                log.info(sql);
             } else {
+                log.error(err);
+            }
 
-                const sql   = 'UPDATE '+ config.database +'.customers ' +
-                        'SET customerName = '+ '"' + req.query.name + '"' +', ' +
-                        'contactLastName = '+ '"' + req.query.lastname + '"' +', ' +
-                        'contactFirstName = '+ '"' + req.query.firstname + '"' +', ' +
-                        'phone = '+ '"' + req.query.phone + '"' +' ' +
-                        'WHERE customers.customerNumber = '+ req.params.number + ';';
 
-                con.query(sql,
-                    function (err) {
-                        if (err) {
-                            console.log(sql);
+            con.query(sql,
+                function (err) {
+                    if (err) {
+                        if (err.code === 'ER_EMPTY_QUERY') {
+                            con.release();
+                            res.status(400);
+                            res.send('Something wrong with a query');
+                            return log.error(err);
+                        } else {
                             con.release();
                             res.send({success: false, error: err});
+                            return log.error(err);
                         }
-                        con.release();
-                        res.send('Customer №'+ req.params.number + ' successfully updated');
                     }
-                )
-            }
+                    con.release();
+                    res.send('Customer №' + req.params.number + ' successfully updated');
+                }
+            )
         })
     };
 
     this.insert = function (req, res) {
         connection.acquire(function (err, con) {
-            if (!req.params.number) {
-                res.status(403);//
-                return res.sendStatus('Access denied')
-            } else {
-                con.query('INSERT INTO ' + config.database + '.customers (customerNumber) VALUES (' + req.params.number + ')',
-                    function (err) {
-                    if (err) {
-                        con.release();
-                        res.send({success: false, error: err});
-                    }
-                        con.release();
-                        res.send('Customer successfully added');
-                    }
-                )
+            log.info('INSERT INTO ' + config.database + '.customers (customerNumber) VALUES (' + con.escape(req.params.number) + ')');
+
+            if (!validator.isNumeric(req.params.number)) {
+                res.status(400);
+                res.send('Enter a number!');
+                con.release();
+                return log.error(err);
             }
+
+            con.query('INSERT INTO ' + config.database + '.customers (customerNumber) VALUES (' + con.escape(req.params.number) + ')',
+                function (err) {
+                    if (err) {
+                        if (err.code === 'ER_DUP_ENTRY') {
+                            res.status(403);
+                            res.send('Customer with this number already exists.');
+                            con.release();
+                            return log.error(err);
+                        }
+                    }
+                    con.release();
+                    res.send('Customer successfully added');
+                }
+            )
         })
     }
 }
--- server/server.js
+++ server/server.js
@@ -33,6 +33,21 @@ app.delete('/customers/:number', function (req, res) {
     Data.delete(req, res);
 });
 
+app.use(function (err, req, res, next) {
+
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    res.type('txt').send('Not found');
+});
+
 
 
 
